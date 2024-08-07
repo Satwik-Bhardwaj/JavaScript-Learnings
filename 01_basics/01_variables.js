4ba8d886a6e6f82dbd831513141457968d612c9f
satwik-bhardwaj <satwikbhardwaj123@gmail.com>
2024-08-07 12:27:25 +0530

    adding my javascript learnings

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,25 @@
+const myAge = 21;
+var myName = "Satwik";
+let myEmail = "satwik@example.com";
+
+
+// change myAge
+// myAge = 22;
+console.log(myAge);
+
+// change myEmail
+myEmail = "hello@example.com";
+console.log(myEmail);
+
+// change myName
+myName = "Bhardwaj";
+console.log(myName);
+
+{
+    console.table([myAge, myEmail, myName]);
+
+    var currentMonth = "August";
+    let currentYear = 2024;
+}
+
+console.table([currentMonth, currentYear]);
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,8 @@
+"use strict";   // this treat all  JS code as newer version
+
+let name = "Satwik";        // string
+let age = 21;               // integer
+let isLoggedIn = true;      // boolean
+let networth = 43896.12;    // float
+let aValue = null;          // null (standalone value)
+let notInit;                // undefined 
