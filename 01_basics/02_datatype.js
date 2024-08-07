088a11ff96e62d69da025b73ace83f9283279e6f
satwik-bhardwaj <satwikbhardwaj123@gmail.com>
2024-08-07 15:14:46 +0530

    adding learning

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -6,3 +6,14 @@ let isLoggedIn = true;      // boolean
 let networth = 43896.12;    // float
 let aValue = null;          // null (standalone value)
 let notInit;                // undefined 
+// symbol => unique
+// object
+
+console.table([
+    [ name, typeof name], 
+    [ age, typeof age], 
+    [ isLoggedIn, typeof isLoggedIn], 
+    [ networth, typeof networth],
+    [ aValue, typeof aValue ],
+    [ notInit, typeof notInit ]
+]);
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+// let score = "33";
+
+// console.log(score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
+
+// let variable = "abc123";
+
+// console.log(variable);
+// console.log(typeof(variable));
+
+// let num = Number(variable);
+// console.log(num);
+// console.log(typeof(num));
+
+let isLoggedIn = 1;
+console.log(isLoggedIn);
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
