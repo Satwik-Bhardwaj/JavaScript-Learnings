8240977fd970951aff92af14eb3faf9d72977b8d
satwik-bhardwaj <satwikbhardwaj123@gmail.com>
2024-08-07 17:37:59 +0530

    Learning more datatypes

---
+++ 01_basics/05_datatypeSummary.js
@@ -0,0 +1,13 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id);
+console.log(id == anotherId)
+
+// Reference (non primitive)
+
+// Array, Objects, Functions
