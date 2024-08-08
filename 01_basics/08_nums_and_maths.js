db59e80e05310b5beefc7be89278492584ee23b0
satwik-bhardwaj <satwikbhardwaj123@gmail.com>
2024-08-08 16:55:00 +0530

    Completing 08_nums_and_maths

---
+++ 01_basics/08_nums_and_maths.js
@@ -0,0 +1,31 @@
+// const score = 99;
+// console.log(score);
+
+// const balance = Number(12243);
+// console.log(balance);
+// console.log("To fixed: ", balance.toFixed(2));
+
+// const otherNumber =  1644.2322;
+// console.log(otherNumber.toPrecision(2));
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString("en-IN"))
+
+// ++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(6.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.max(3, 2 ,1, 7));
+
+// Random ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10) + 1);
+let min = 10;
+let max = 20;
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
+
